5e214fa930d49fe6743a392ca0ddcb7fda51e7f8
Rodrigo Fernandes da Costa <rodrigo027-0@yahoo.com.br>
2011-09-07 14:30:28 -0300

    modularization and parser update

--- main.c
+++ main.c
@@ -10,165 +10,31 @@
 
 int main(void)
 {
-    char command_matrix[COMMAND_MATRIX_HEIGHT][COMMAND_MATRIX_WIDTH] = {0};
-    char c;
-    int i,j;
-    int temp;
-
+    t_token token = {0};
+    t_command command = {0};
+    t_boolean end = false;
+    t_boolean echo_input = ECHO_INPUT;
+    
     do
     {
-        /* clean commmad matrix */
-        j = i = 0;
-        memset(command_matrix,'\0',COMMAND_MATRIX_HEIGHT*COMMAND_MATRIX_WIDTH);
-        
-        printf("shell> ");
-        
-        /* get user input */
-        while((c = getchar()) != '\n' && i < COMMAND_MATRIX_HEIGHT - 1)
+        f_get_tokens(&token);
+        f_identify_tokens(&token);
+        if(!f_check_syntax(&token)) continue;
+        if(ECHO_TOKENS) f_echo_tokens(&token);
+        if(echo_input) f_echo_input(&token);
+        while(f_get_command(&token,&command))
         {
-            switch (c)
+            switch(command.type)
             {
-                case '\t':
-                case ' ':
-                    if(j != 0)
-                    {
-                        ++i;
-                        j = 0;
-                    }
-                    break;
-                default:
-                    if(j < COMMAND_MATRIX_WIDTH - 1)
-                    {
-                        command_matrix[i][j++] = c;
-                    }
+                case exit: end = f_exit(command); break;
+                case quit: end = f_quit(command); break;
+                case echo: f_echo(command); break;
+                case test: f_test(command); break;
+                case system: f_system(command); break;
             }
         }
-        
-        /* clean redirection commands */
-        for(i = 0 ; i < COMMAND_MATRIX_HEIGHT ; ++i)
-        {
-            if
-            (
-                command_matrix[i][0] == '|'
-                && command_matrix[i][1] == '\0'
-            )
-            {
-                command_matrix[i][0] = '\0';
-            }
-            if
-            (
-                (
-                    command_matrix[i][0] == '<'
-                    && command_matrix[i][1] == '\0'
-                ) || (
-                    command_matrix[i][0] == '>'
-                    && command_matrix[i][1] == '\0'
-                ) || (
-                    command_matrix[i][0] == '<'
-                    && command_matrix[i][1] == '<'
-                    && command_matrix[i][2] == '\0'
-                ) || (
-                    command_matrix[i][0] == '>'
-                    && command_matrix[i][1] == '>'
-                    && command_matrix[i][2] == '\0'
-                )
-            )
-            {
-                command_matrix[i][0] = '\0';
-                command_matrix[i][1] = 'f';
-            }
-        }
-        
-        /* print user input without redirection commands */
-        // for(printf("echo>") , i = 0 ; i < COMMAND_MATRIX_HEIGHT ; ++i)
-        // {
-            // if(command_matrix[i][0] != '\0')
-            // {
-                // printf(" %s",command_matrix[i]);
-            // }
-        // }
-        // putchar('\n');
-        // for(i = 0 ; i < COMMAND_MATRIX_HEIGHT ; ++i)
-        // {
-            // if (command_matrix[i][0] == '\0')
-                // printf("NOTHING");
-            // else
-                // printf("%s",command_matrix[i]);
-            // putchar('\n');
-        // }
-        
-        /* look for command */
-        
-        i = 0;
-        while(i < COMMAND_MATRIX_HEIGHT)
-        {
-            /* co command found */
-            if(command_matrix[i][0] == '\0')
-            {
-                ++i;
-                continue;
-            }
-        
-            if(! strcmp("exit",command_matrix[i]) )
-            {
-                if(i+1 < COMMAND_MATRIX_HEIGHT && command_matrix[i+1][0] == '\0')
-                {
-                    printf("shell> quit\n");
-                    return 0;
-                }
-                temp = 0;
-                if
-                (
-                    i+1 < COMMAND_MATRIX_HEIGHT && 
-                    (! sscanf(command_matrix[i+1],"%d",&temp))
-                )
-                {
-                    printf("?> Command parameter not reconized.\n");
-                    printf("?> type: exit return_value(optional)\n");
-                }
-                else
-                {
-                    printf("shell> quit\n");
-                    return temp;
-                }
-            }
-            
-            else if(! strcmp("quit",command_matrix[0]))
-            {
-                if(i+1 < COMMAND_MATRIX_HEIGHT && command_matrix[i+1][0] == '\0')
-                {
-                    printf("shell> quit\n");
-                    return 0;
-                }
-                temp = 0;
-                if
-                (
-                    i+1 < COMMAND_MATRIX_HEIGHT && 
-                    (! sscanf(command_matrix[i+1],"%d",&temp))
-                )
-                {
-                    printf("?> Command parameter not reconized.\n");
-                    printf("?> type: quit return_value(optional)\n");
-                }
-                else
-                {
-                    printf("shell> quit\n");
-                    return temp;
-                }
-            }
-            
-            else
-            {
-                printf("?> command \"%s\" not found.\n",command_matrix[i]);
-            }
-            
-            while(command_matrix[i][0] != '\0' || command_matrix[i][1] == 'f')
-            {
-                ++i;
-            }
-            ++i;
-        }
-    }while(1);
+    }
+    while(!end);
     
 	return 0;
 }
--- main.h
+++ main.h
@@ -1,6 +1,74 @@
+/* 
+    Nome: Rodrigo Fernandes da Costa
+    n. USP: 6793073
+    
+    Nome: Lucas de Moraes Franco
+    n. USP: 6793239
+*/
+
 #include <stdio.h>
 #include <string.h>
 
-#define COMMAND_MATRIX_WIDTH 10
-#define COMMAND_MATRIX_HEIGHT 10
+#define TOKEN_STRING_SIZE 10
+#define ECHO_TOKENS true
+#define ECHO_INPUT true
 
+typedef enum boolean {false,true} t_boolean;
+
+typedef char t_token_string[TOKEN_STRING_SIZE];
+
+/* word can be a command, argument or file, it is mark for further process */
+typedef enum
+{
+    command,argument,file,word,input,output,append,pipe,background
+}
+token_type;
+
+/* linked list of tokens, the first token is always invalid (head) */
+struct token
+{
+    token_type type;
+    t_token_string a_string;
+    struct token * a_next;
+};
+
+typedef struct token t_token;
+
+/* linked list of arguments, the first argument is always valid */
+struct argument
+{
+    t_token_string a_string;
+    struct argument * a_next;
+};
+
+typedef struct argument t_argument;
+    
+struct command
+{
+    enum {quit,exit,echo,test,system} type; /* system for not built in commands */
+    t_argument argument; /* first argument is the name of the command */
+    t_token_string a_input; /* empty string when not redirected */
+    t_token_string a_output; /* empty string when not redirected */
+    t_token_string a_append; /* empty string when not redirected */
+    t_boolean pipe_input;
+    t_boolean pipe_output;
+    t_boolean background;
+};
+
+
+typedef struct command t_command;
+
+/* parser */
+void f_get_tokens(t_token * a_head);
+void f_identify_tokens(t_token * a_head);
+t_boolean f_check_syntax(t_token * a_head);
+t_boolean f_get_command(t_token * a_head,t_command * a_command); /* returns true when there are one or more commands */
+void f_echo_tokens(t_token * a_head);
+void f_echo_input(t_token * a_head);
+
+/* commands */
+t_boolean f_exit(t_command command);
+t_boolean f_quit(t_command command);
+void f_echo(t_command command);
+void f_system(t_command command);
+void f_test(t_command command);
